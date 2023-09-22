a1aa5d889668e3e2dcd1cbd6b77360fd48e917d9
Januelle Asturias Abion <145733675+asturias-jnll@users.noreply.github.com>
2023-09-22 12:00:36 +0800

    Add files via upload

---
+++ Activity2_No2.py
@@ -0,0 +1,16 @@
+eng = float(input("Enter your grade in English: "))
+fil = float(input("Enter your grade in Filipino: "))
+sci = float(input("Enter your grade in Science: "))
+math = float(input("Enter your grade in Math: "))
+ap = float(input("Enter your grade in Araling Panlipunan: "))
+pe = float(input("Enter your grade in Physical Education: "))
+
+ave = (eng + fil + sci + math + ap + pe)/6
+
+print("English: " , eng)
+print("Filipino: " , fil)
+print("Science: " , sci)
+print("Math: " , math)
+print("Araling Panlipunan: " , ap)
+print("Physical Education: " , pe)
+print("Average: " , ave)
