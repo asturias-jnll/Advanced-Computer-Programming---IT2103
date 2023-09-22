ba27e60199fe74f3a369de6946638ebbc40b059f
Januelle Asturias Abion <145733675+asturias-jnll@users.noreply.github.com>
2023-09-22 11:37:03 +0800

    Add files via upload

---
+++ Activity1_No2.py
@@ -0,0 +1,7 @@
+name = input("Enter name: ")
+birthyear = int(input("Enter birthyear: "))
+currentyear = int(input("Enter current year: "))
+age = currentyear - birthyear
+
+print("Name: " + name)
+print("Age: ", age)
